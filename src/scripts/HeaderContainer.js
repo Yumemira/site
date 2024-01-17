a94984736720db54da8d59723bb94e017f432400
Yumemira <surrenderyumi@mail.ru>
2024-01-17 20:27:18 +0300

    txt

--- src/scripts/HeaderContainer.js
+++ src/scripts/HeaderContainer.js
@@ -2,7 +2,7 @@ import '../styles/headerContainerStyle.css';
 
 export default function HeaderMenu(){
     return <div id="header--head">
-        <a className='header--head-text' href='#'>Пишите по любому вопросу</a>
+        <p className='header--head-text'>Пишите по любому вопросу</p>
         <p className='header--head-text'>+7(012)3456789</p>
     </div>
 }
